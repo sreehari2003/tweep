45c27e88a6e36d0b1e19d8c1e3aea8a2080ab1ea
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-07-30 20:47:24 +0530

    fix: loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,40 +3,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Toaster } from "sonner";
 import { AuthCtx } from "@app/context/auth";
-import { useState } from "react";
-import Router from "next/router";
-import { Loader } from "@app/components";
+import { useEffect, useState } from "react";
 import { DefaulSeo } from "@app/components/DefaulSeo";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isLoading, setLoading] = useState(false);
-
-  // listening for route change events
-  Router.events.on("routeChangeStart", () => {
-    // when route change loading screen popup
-    setLoading(true);
-  });
-  Router.events.on("routeChangeComplete", () => {
-    setLoading(false);
-  });
-
-  if (isLoading) {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <AuthCtx>
-          <DefaulSeo />
-          <Toaster />
-          <Loader />
-          <div className="d-none">
-            <Component {...pageProps} />
-          </div>
-        </AuthCtx>
-      </QueryClientProvider>
-    );
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
       <AuthCtx>
